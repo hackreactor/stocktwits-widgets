6e1d7a96de9f25b46313902607cf354a57bf7437
Selby Walker <selby.e.walker@gmail.com>
2013-05-03 16:12:16 -0700

    html and script combined

--- embeddable-messages/script.js
+++ embeddable-messages/script.js
@@ -28,7 +28,7 @@ var JSONP = function(url, callback) {
 
 
 function showTwit(messageId) { 
-  var messageId = 13337309; // delete when passing in from HTML
+  var messageId = 13357309; // delete when passing in from HTML
   var frame = document.createElement("iframe");
 
   // Find the last element in the DOM
@@ -57,11 +57,11 @@ function showTwit(messageId) {
         'July', 'August', 'September', 'October', 'November', 'December'];
 
     //inner text cross browser to avoid XSS .innerText
-    $('.embedded-username').html(messageInfo.message.user.username);
-    $('.embedded-dateline').html(months[date.getMonth()] + ' ' + date.getDate() + ' at ' 
+    $('.embedded_st-username').html(messageInfo.message.user.username);
+    $('.embedded_st-dateline').html(months[date.getMonth()] + ' ' + date.getDate() + ' at ' 
       + hours + ':' + date.getMinutes() + ' ' + amOrPm);
-    $('.embedded-entry-title').html(messageInfo.message.body);
-    $('.embedded-avatar').html('<img src="' + messageInfo.message.user.avatar_url +'"/>');
+    $('.embedded_st-entry-title').html(messageInfo.message.body);
+    $('.embedded_st-avatar').html('<img src="' + messageInfo.message.user.avatar_url +'"/>');
   });
 }
 // Expose under namespace
