4f2b50ac1bcda84cd2e78d54c89af1c9adca60e8
Selby Walker <selby.e.walker@gmail.com>
2013-05-03 15:22:10 -0700

    script fetching from api

--- embeddable-messages/script.js
+++ embeddable-messages/script.js
@@ -1,47 +1,71 @@
+var _ST = _ST || {};
+
 (function() {
 
-var jQuery;
+function require(src) {
+  var doc = document.documentElement;
+  var script = document.createElement('script');
+  script.src = src;
+  doc.removeChild(doc.insertBefore(script, doc.firstChild));
+};
 
-//Load jQuery if not already loaded
-if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
-    var script_tag = document.createElement('script');
-    script_tag.setAttribute("type","text/javascript");
-    script_tag.setAttribute("src",
-        "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-    if (script_tag.readyState) {
-      script_tag.onreadystatechange = function () { // For old versions of IE
-          if (this.readyState == 'complete' || this.readyState == 'loaded') {
-              scriptLoadHandler();
-          }
-      };
-    } else { // Other browsers
-      script_tag.onload = scriptLoadHandler;
+var JSONP = function(url, callback) {
+  var GLOBALCALLBACKNAME = 'st_' + Date.now() + '_' + Math.floor(Math.random()*100);
+  window[GLOBALCALLBACKNAME] = function(data){
+    callback(data);
+    //clear timeout
+    };
+  // Insert a script tag into the DOM with the URL + callback=GLOBALCALLBACKNAME
+    // document.createElement('script')
+
+  require(url + '?callback=' + GLOBALCALLBACKNAME);
+
+  // append to head
+
+  // Optional: Set a timeout for X seconds to "abort" the request
+    // var myTimeout = setTimeout(func, 1000);
+  // Clean the timeout (if you did that)
+    // clearTimeout(myTimeout);
+};
+
+
+function showTwit(messageId) { 
+  var messageId = 13337309; // delete when passing in from HTML
+  var frame = document.createElement("iframe");
+
+  // Find the last element in the DOM
+  // Insert an iframe after it
+  // Setup the iframe document with html element and remove border and creat size
+  // Set up document inside iframe, css-document.write
+  // WARNING: inserting style tags does not work for IE
+  // Set iframe's body element's HTML to your template
+  // After the response is recieved, you need to query the iframe (using getElementByTagName), find the places where info goes, and use innerText
+
+  var template = '<div class="_ST_embed_username"></div>';
+
+  JSONP("https://api.stocktwits.com/api/2/messages/show/" + messageId + ".json", function(messageInfo){
+    console.log(messageInfo)
+    var date = new Date(messageInfo.message.created_at);
+    var amOrPm = date.getHours() > 11 ? 'PM' : 'AM';
+    var hours = date.getHours();
+    if(amOrPm == 'AM'){
+      if(hours == 0) {
+        hours = 12;
+      }
+    } else {
+      hours = hours - 12;
     }
-    // Try to find the head, otherwise default to the documentElement
-    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
-} else {
-    // The jQuery version on the window is the one we want to use
-    jQuery = window.jQuery;
-    main();
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+
+    //inner text cross browser to avoid XSS .innerText
+    $('.embedded-username').html(messageInfo.message.user.username);
+    $('.embedded-dateline').html(months[date.getMonth()] + ' ' + date.getDate() + ' at ' + hours + ':' + date.getMinutes() + ' ' + amOrPm);
+    $('.embedded-entry-title').html(messageInfo.message.body);
+  });
 }
+// Expose under namespace
+_ST.showTwit = showTwit;
 
-
-function scriptLoadHandler() {
-    jQuery = window.jQuery.noConflict(true);
-    main(); 
-}
-
-function main() { 
-    jQuery(document).ready(function($) { 
-      var messageId = 13336305;
-      jQuery.ajax("https://api.stocktwits.com/api/2/messages/show/" + messageId + ".json").done(function(data){
-        var messageInfo = JSON.parse(data);
-        $('.name').html()
-      }  
-    });
-
-
-    });
-}
-
-})(); 
+}()); 
