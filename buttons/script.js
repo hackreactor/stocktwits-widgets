0c292551b4355d8fb7f5c338ef8dea07887b17b1
Blake Embrey <hello@blakeembrey.com>
2013-05-06 15:27:13 -0700

    Update buttons.

--- buttons/script.js
+++ buttons/script.js
@@ -1,4 +1,28 @@
 (function (attributes) {
+  var __slice = function (object, from) {
+    return Array.prototype.slice.call(object, from || 0);
+  };
+
+  var getElementsByClassName; // Function to get class names
+
+  if (document.getElementsByClassName) {
+    getElementsByClassName = function (className) {
+      return __slice(document.getElementsByClassName(className));
+    };
+  } else {
+    getElementsByClassName = function (className) {
+      var found    = [],
+          regex    = new RegExp('(^| )' + className + '($| )'),
+          elements = document.getElementsByTagName('*');
+      for (var i = 0; i < elements.length; i++) {
+        if (regex.test(elements[i].className)) {
+          found.push(elements[i]);
+        }
+      }
+      return found;
+    };
+  }
+
   var generateQueryString = function (query) {
     var string = '';
     for (var key in query) {
@@ -9,13 +33,17 @@
     return string;
   };
 
-  var createButton = function (anchor) {
+  var createButton = function (element) {
     var iframe = document.createElement('iframe'),
         query  = {};
 
     for (var i = 0; i < attributes.length; i++) {
-      query[attributes[i]] = anchor.getAttribute('data-' + attributes[i]);
+      query[attributes[i]] = element.getAttribute('data-' + attributes[i]);
     }
+
+    // Page information
+    query.pageUrl   = window.location.href;
+    query.pageTitle = document.title;
 
     var queryString = generateQueryString(query);
 
@@ -23,16 +51,16 @@
     iframe.scrolling = 0;
     iframe.width     = 180;
     iframe.height    = 20;
+    iframe.setAttribute('frameborder',       0);
     iframe.setAttribute('allowtransparency', 'true');
-    iframe.setAttribute('frameborder', 0);
 
-    console.log(iframe);
+    element.parentNode.replaceChild(iframe, element);
   };
 
-  var anchors = document.getElementsByClassName('stocktwits-share-button');
+  var elements = getElementsByClassName('stocktwits-share-button');
 
-  for (var i=0; i<anchors.length; i++) {
-    createButton(anchors[i]);
+  for (var i = 0; i < elements.length; i++) {
+    createButton(elements[i]);
   }
 
 })(['follow', 'mention', 'cashtag']);
