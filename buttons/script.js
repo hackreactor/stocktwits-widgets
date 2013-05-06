c2854f5c0436cc55962cf177723e595d346f7043
Psyduk <thestryka@gmail.com>
2013-05-06 12:24:15 -0700

    Created iFrame Elements

--- buttons/script.js
+++ buttons/script.js
@@ -1,3 +1,38 @@
-(function () {
+(function (attributes) {
+  var generateQueryString = function (query) {
+    var string = '';
+    for (var key in query) {
+      if (!query.hasOwnProperty(key) || !query[key]) { continue; }
+      if (string) { string += '&'; }
+      string += encodeURI(key) + '=' + encodeURIComponent(query[key]);
+    }
+    return string;
+  };
 
-})();
+  var createButton = function (anchor) {
+    var iframe = document.createElement('iframe'),
+        query  = {};
+
+    for (var i = 0; i < attributes.length; i++) {
+      query[attributes[i]] = anchor.getAttribute('data-' + attributes[i]);
+    }
+
+    var queryString = generateQueryString(query);
+
+    iframe.src       = 'buttons/index.html?' + queryString;
+    iframe.scrolling = 0;
+    iframe.width     = 180;
+    iframe.height    = 20;
+    iframe.setAttribute('allowtransparency', 'true');
+    iframe.setAttribute('frameborder', 0);
+
+    console.log(iframe);
+  };
+
+  var anchors = document.getElementsByClassName('stocktwits-share-button');
+
+  for (var i=0; i<anchors.length; i++) {
+    createButton(anchors[i]);
+  }
+
+})(['follow', 'mention', 'cashtag']);
