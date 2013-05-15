e94d514aa091adee66f676a7c82330f338c6de78
Blake Embrey <hello@blakeembrey.com>
2013-05-15 15:48:45 -0700

    Adding link to swf.

--- embeddable-messages/widget.js
+++ embeddable-messages/widget.js
@@ -29,6 +29,7 @@
     var messageId = parseInt(element.getAttribute('data-id'), 10);
 
     new easyXDM.Socket({
+      swf:    'http://hackreactor.github.io/stocktwits-widgets/easyXDM/easyxdm.swf',
       remote: 'http://hackreactor.github.io/stocktwits-widgets/embeddable-messages/index.html?messageID=' + messageId,
       container: element,
       onMessage: function (height) {
