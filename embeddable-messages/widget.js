5d203eb619d55d19bdd0062db4cd2916ecf7e97a
Blake Embrey <hello@blakeembrey.com>
2013-05-17 18:19:48 -0700

    Alter code once again.

--- embeddable-messages/widget.js
+++ embeddable-messages/widget.js
@@ -32,16 +32,15 @@
     new easyXDM.Socket({
       swf:    'http://hackreactor.github.io/stocktwits-widgets/easyXDM/easyxdm.swf',
       remote: 'http://hackreactor.github.io/stocktwits-widgets/embeddable-messages/index.html?messageID=' + messageId,
-      container: element,
+      container: element.parentNode,
       onMessage: function (height) {
         iframe.height = height;
       },
       onReady: function () {
+        element.parentNode.removeChild(element);
         // This is kind of messed up, but we basically promote the easyXDM iframe
         // up the dom and apply styling
-        iframe = element.getElementsByTagName('iframe')[0];
-
-        this.container = iframe; // This is kind of screwed up
+        iframe = this.container.getElementsByTagName('iframe')[0];
 
         iframe.style              = {};
         iframe.style.display      = 'block';
@@ -58,10 +57,10 @@
         iframe.setAttribute('border',            '0');
         iframe.setAttribute('frameborder',       '0');
         iframe.setAttribute('allowtransparency', 'true');
-
-        element.parentNode.replaceChild(iframe, element);
       }
     });
+
+
   };
 
   var elements = getElementsByClassName('stocktwit-twit');
